e11b807df37dc037217ea28fc2256dd7a87419da
Luca Scalzotto <luca@scorpiac.com>
2023-12-19 17:06:19 +0100

    Working geo zone configuration

--- tests/configure.setup.ts
+++ tests/configure.setup.ts
@@ -4,5 +4,18 @@ import { navigateAdmin } from './helpers/navigate';
 test('configure geo zones', async ({ page }) => {
   await navigateAdmin(page, 'localisation/geo_zone');
 
-  // TODO: Check if NL zone exists, create it.
+  if (await page.getByText('NL Shipping').count() === 0) {
+    await navigateAdmin(page, 'localisation/geo_zone.form');
+
+    // Fill the name and description.
+    await page.getByLabel('Geo Zone Name').fill('NL Shipping');
+    await page.getByLabel('Description').fill('Netherlands');
+
+    // Add a country, select "Netherlands".
+    await page.locator('#button-geo-zone').click();
+    await page.locator('[name="zone_to_geo_zone[0][country_id]"]').selectOption('Netherlands');
+
+    // Save the geo zone.
+    await page.locator('button[type="submit"]').click();
+  }
 });
